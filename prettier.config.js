0d58e7811f2cbe14ac4cf0f058e52d66d74c0a11
Siddharth <siddharth@Siddharths-MacBook-Pro.local>
2021-06-16 15:37:43 +0530

    build(prettier): configuring prettier
    added .prettierignore & prettier.config.json to the project

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  htmlWhitespaceSensitivity: "css",
+  printWidth: 80,
+  insertPragma: false,
+  proseWrap: "always",
+  quoteProps: "as-needed",
+  requirePragma: false,
+  tabWidth: 2,
+  trailingComma: "all",
+  useTabs: false,
+};
