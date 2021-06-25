d3abc30fbdb59a7bf83b45f879bd8b679566cfdb
Siddharth <siddharth@Siddharths-MacBook-Pro.local>
2021-06-26 01:50:47 +0530

    refactor(index page): removed unnecessary components from index page & added Hero component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,33 +1,12 @@
-import { css } from "@emotion/react";
 import React from "react";
 import Layout from "../components/layout";
-import styled from "@emotion/styled";
 
-const StyledHeading = styled.h1`
-  color: ${(props) => (props.theme ? props.theme.primaryColor : "#000000")};
-  font-family: "IBM Plex Sans", sans-serif;
-`;
-
-const StyledParagraph = styled.p`
-  color: ${(props) => (props.theme ? props.theme.secondaryColor : "#000000")};
-  font-family: "IBM Plex Mono", monospace;
-  font-size: 14px;
-`;
+import Hero from "../components/hero";
 
 const Index = () => {
   return (
     <Layout>
-      <StyledHeading
-        css={css`
-          padding-top: 20px;
-          margin: 0;
-        `}
-      >
-        Welcome to Sid&rsquo;s portfolio!
-      </StyledHeading>
-      <StyledParagraph>
-        This is some text regarding siddharth s kumar.
-      </StyledParagraph>
+      <Hero />
     </Layout>
   );
 };
