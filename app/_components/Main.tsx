7d13373cbcda52b1745d84239e563c3b4299c8fb
Siddharth S Kumar <siddharthskumar@MacBook-Pro.local>
2023-08-09 15:05:18 +0530

    feature(): implement main content structure

---
+++ app/_components/Main.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Introduction from './Introduction';
+import About from './About';
+import Experience from './Experience';
+import Contact from './Contact';
+
+/**
+ * TODO: skills.
+ * TODO: projects.
+ */
+
+export default function Main() {
+  return (
+    <main>
+      <Introduction />
+      <About />
+      <Experience />
+      <Contact />
+    </main>
+  );
+}
